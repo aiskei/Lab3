b6a92f760c68ccefc4b083574894d348916f1c44
Aisha Keita <aishacassel@gmail.com>
2024-07-11 14:16:20 +0200

    Add electronic page Fetch data and display products

---
+++ src/app/users/electronics/page.jsx
@@ -0,0 +1,14 @@
+"use client";
+import React from 'react'
+import Electronics from '@/app/components/Electornics';
+
+const UserElectronicPage = () => {
+
+    return (
+        <div>
+            <Electronics />
+        </div>
+    )
+}
+
+export default UserElectronicPage
